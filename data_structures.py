ecbd06384cf9ac41537437e6642591d9a980d3c1
naveen-1515 <63184144+naveen-1515@users.noreply.github.com>
2020-04-06 12:02:22 +0530

    data_structures.py
    printing list , tuple, dictionary

---
+++ data_structures.py
@@ -0,0 +1,15 @@
+list1=[1,2,3,4,5]
+list2=[10,11,12,13]
+print(list1)
+print(list2)
+list1.append(6)
+list2.append(14)
+print(list1)
+print(list2)
+tuple=(2,4,6,8)
+print(tuple)
+print(tuple[3])
+dict={"john":"56","naveen":"75","ram":"88","vijay":"95"}
+print(dict)
+del dict["ram"]
+print(dict)
